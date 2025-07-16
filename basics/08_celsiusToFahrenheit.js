35b5691d7c820f3b11500ab81410e5a404006ad9
Ankit Singh <singhankit0033@gmail.com>
2025-07-16 16:13:34 +0530

    add question 8

---
+++ basics/08_celsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+// 8. Create a function that converts Celsius to Fahrenheit.
+
+function celsiusToFahrenheir(temp){
+	let fah = (temp * (9/5)) + 32;
+	return fah;	
+};
+
+console.log(celsiusToFahrenheir(40.7))
